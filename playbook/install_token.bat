0812ad6e9fa73e591606d3795c40c41c11bc6d6e
riasol <riasol.rob@gmail.com>
2012-10-18 21:46:15 +0200

    cleanup

--- playbook/install_token.bat
+++
@@ -1,6 +0,0 @@
-THIS
-java com.qnx.bbt.airpackager.BarAirPackager -cp@{sdk}/lib/EccpressoJDK15ECC.jar;@{sdk}/lib/EccpressoAll.jar;@{sdk}/lib/BarSigner.jar;@{sdk}/lib/BarDeploy.jar;@{sdk}/lib/BarAir.jar;@{sdk}/lib/BarPackager.jar"
--installDebugToken debug.bar
-				<arg line="'${user.home}/Research In Motion/'" />
-				<arg line="-device @{ip}" />
-				<arg line="-password @{password}
